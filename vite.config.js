ce3ac97b83617b57db566634dd13e9c13a9841d9
savi1411 <carlosalberto.savi@gmail.com>
2025-05-16 18:05:06 -0300

    Corrigir uso de require no vite.config.js para compatibilidade ESM

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Correção para __dirname em ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   plugins: [reactRefresh()],
 });
