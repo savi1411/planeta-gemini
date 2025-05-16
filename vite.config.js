d3f32b64676159120a4a5056d2883c681ff888be
savi1411 <carlosalberto.savi@gmail.com>
2025-05-16 18:12:44 -0300

    Corrige plugin do Vite para usar @vitejs/plugin-react

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Correção para __dirname em ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  plugins: [reactRefresh()],
+  plugins: [react()],
 });
